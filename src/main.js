cc524a7ed1467c5abad9fe079af3294fdc2774e7
hyobin86 <127078893+hyobin86@users.noreply.github.com>
2023-06-01 13:02:13 +0900

    클립보드 공유 기능

--- src/main.js
+++ src/main.js
@@ -5,4 +5,13 @@ import { createPinia } from 'pinia';
 
 import 'vue-global-api'; //전역 api 지정
 
-createApp(App).use(router).use(createPinia()).mount('#app');
+import VueClipboard from 'vue3-clipboard'; //vue3 용 클립보드
+
+createApp(App)
+  .use(router)
+  .use(VueClipboard, {
+    autoSetContainer: true,
+    appendToBody: true,
+  })
+  .use(createPinia())
+  .mount('#app');
--- src/view/page1.vue
+++ src/view/page1.vue
@@ -1,16 +1,58 @@
 <template lang="pug">
 .page1
   h2 복사 및 sns 공유
+  button(type="button" @click="getShare('twitter')") 트위터
+  button(type="button" @click="getShare('facebook')") 페이스북
+  button(type="button" @click="getShare('copy')") 복사하기
+  span.tag(@click="getCopy('#복사할단어')") #복사할단어
 </template>
 
 <script setup>
-import BaseCom from '@/components/BaseCom.vue';
+import { copyText } from 'vue3-clipboard';
 
-const msg = ref('template');
-onMounted(() => {});
+let thisUrl = 'www.naver.com';
+let snsTitle = '복사테스트!!';
+function getShare(sns) {
+  if (sns == 'facebook') {
+    let url =
+      'http://www.facebook.com/sharer/sharer.php?u=' +
+      encodeURIComponent(thisUrl);
+    window.open(url, '페이스북', 'width=486, height=286');
+  } else if (sns == 'twitter') {
+    let url =
+      'http://twitter.com/share?url=' +
+      encodeURIComponent(thisUrl) +
+      '&text=' +
+      encodeURIComponent(snsTitle);
+    window.open(url, '트위터', 'width=486, height=286,scrollbars=yes');
+  } else {
+    getCopy(thisUrl);
+  }
+}
+function getCopy(obj) {
+  copyText(obj, undefined, (error, event) => {
+    if (error) {
+      alert('복사가 실패했습니다.');
+    } else {
+      alert('복사되었습니다.');
+    }
+  });
+}
 </script>
 
 <style lang="less">
 .page1 {
+  button {
+    background: transparent;
+    font-size: 15px;
+    margin: 10px 5px;
+    padding: 6px 10px;
+  }
+  .tag {
+    cursor: pointer;
+    &:hover {
+      color: red;
+    }
+  }
 }
 </style>
